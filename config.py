f1a8d4febd229b3bf8751c94d99958da4629fecb
Dreamteamdbl <amazingyaan@gmail.com>
2025-04-16 12:49:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = os.environ.get("API_ID", "12475131")
 
-API_HASH = os.environ.get("API_HASH", "719171e38be5a1f500613837b79c536f)
+API_HASH = os.environ.get("API_HASH", "719171e38be5a1f500613837b79c536f")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7673015455:AAFzEP1Zy1EV2BGa8KhEYjoQ1ET0qHr2g4g")
 
@@ -16,10 +16,10 @@ LOG = -1002565842447,
 
 # auth_chats = []
 
-try:
-    ADMINS=[6133985472]
-    for x in (os.environ.get("ADMINS", "6133985472").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
+#try:
+    #ADMINS=[6133985472]
+   # for x in (os.environ.get("ADMINS", "6133985472").split()):
+       # ADMINS.append(int(x))
+#except ValueError:
+        #raise Exception("Your Admins list does not contain valid integers.")
+#ADMINS.append(OWNER)
